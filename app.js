0f9f34f3624c3e5061b1ea4b83f56b2117ffbeb6
jamesmaskell <jameslmaskell@gmail.com>
2020-06-16 18:05:24 +0100

    added ask to remote json file

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
 'use strict'
+import { dataService } from 'dataService.js';
+
 $(document).ready(function() {
     
-    getQuote();
+    let ds = new dataService();
+    
+    function getQuotes(data) {
+        let obj;
+        if (typeof(data) === "string") obj = JSON.parse(data)
+        if (obj.hasOwnProperty("quotes")) {
+            let quoteObj = obj.quotes[Math.floor(Math.random() * obj.quotes.length)] 
 
-    function getQuote() {
-        let quoteArray = random();
-        $("#text").text(quoteArray[0]);
-        $("#author").text(quoteArray[1]);
-        $("#tweet-quote").attr("href", `http://twitter.com/intent/tweet?text=${encodeURI(quoteArray[0])}`)
+            $("#text").text(quoteObj.quote);
+            $("#author").text(quoteObj.author);
+            $("#tweet-quote").attr("href", `http://twitter.com/intent/tweet?text=${encodeURI(quoteObj.quote)}`)
+        }
     }
+
+    function throwErrorToConsole() {
+        console.log("errors")
+    }
+
+    ds.makeAPICall(getQuotes(), throwErrorToConsole())
 
     $("#new-quote").click(function() {
-        getQuote()
-    });
-
-    /* replace this function with an actual call to an API */
-    function random() {
-        /*const quotes = [
-            ["I'm only here so I won't get fined", "Beast Mode"],
-            ["If you’re not gonna go ALL the way, why go at ALL?", "Joe Namath"],
-            ["GO HAWKS!", "Russell Wilson"]
-        ] 
-        let i = Math.floor(Math.random() * 3);
-        console.log("i",i)
-        return quotes[i];*/
-
-        $.get("data.json", function(data, status){
-            if (data.hasOwnProperty("quotes")) {
-                let i = Math.floor(Math.random() * data.quotes.length);
-            }
-        })
-    }
-    
-    
+        ds.makeAPICall(getQuotes(), throwErrorToConsole())
+    });   
 
 });
